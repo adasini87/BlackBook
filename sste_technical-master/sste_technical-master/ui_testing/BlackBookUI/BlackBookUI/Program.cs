421a5ad1718d771d5eec02e3c4330621524a7376
adasini87 <71195008+adasini87@users.noreply.github.com>
2020-09-13 16:10:07 -0400

    Entry creation
    Create Computer Entry completed

--- sste_technical-master/sste_technical-master/ui_testing/BlackBookUI/BlackBookUI/Program.cs
+++ sste_technical-master/sste_technical-master/ui_testing/BlackBookUI/BlackBookUI/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BlackBookUI
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+        // Browser Reference
+        IWebDriver webDriver = new ChromeDriver();
+        
+        public static void Main(string[] args)        {
+
+            //IWebDriver webDriver = new ChromeDriver();
+            //SetValue sv = new SetValue();
+            
+            ////Call inherited methods and pass values
+            //sv.ButtonClick(webDriver, "add", "Id");
+            //sv.EnterValue(webDriver, "/html/body/section/form/fieldset/div[1]/div/input", "BlackBookUITest", "XPath");
+            ////Thread.Sleep(2000);
+            //sv.EnterValue(webDriver, "introduced", "2020-09-01", "Id");
+            //sv.EnterValue(webDriver, "discontinued", "2021-09-01", "Id");
+            //sv.DropDownSelect(webDriver, "company", "Evans & Sutherland", "Id");
+            //sv.ButtonClick(webDriver, "/html/body/section/form/div/input", "XPath");
         }
+
+        [SetUp]
+        public void Launch()
+        {
+            webDriver.Navigate().GoToUrl("https://computer-database.herokuapp.com/computers");
+        }
+
+        [Test]
+        public void UITest()
+        {
+            //Call inherited methods and pass values
+            SetValue.ButtonClick(webDriver, "add", "Id");
+            Thread.Sleep(2000);
+            SetValue.EnterValue(webDriver, "/html/body/section/form/fieldset/div[1]/div/input", "BlackBookUITest", "XPath");
+            //Thread.Sleep(2000);
+            SetValue.EnterValue(webDriver, "introduced", "2020-09-01", "Id");
+            SetValue.EnterValue(webDriver, "discontinued", "2021-09-01", "Id");
+            SetValue.DropDownSelect(webDriver, "company", "Evans & Sutherland", "Id");
+            SetValue.ButtonClick(webDriver, "/html/body/section/form/div/input", "XPath");
+        }
+
+        //[TearDown]
+        //public void Clear()
+        //{            
+        //    webDriver.Close();
+        //}
     }
 }
---
+++ sste_technical-master/sste_technical-master/ui_testing/BlackBookUI/BlackBookUI/SetValue.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium.Support.UI;
+
+namespace BlackBookUI
+{
+    class SetValue
+    {
+        //Pass Text
+        public static void EnterValue(IWebDriver driver, string element,string value,string elementType)
+        {
+            if(elementType=="Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            else if (elementType == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            else if (elementType == "XPath")
+                driver.FindElement(By.XPath(element)).SendKeys(value);
+        }
+
+        //Click button
+        public static void ButtonClick(IWebDriver driver, string element,string elementtype)
+        {
+            if(elementtype=="Id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+            if (elementtype == "XPath")
+                driver.FindElement(By.XPath(element)).Click();
+
+        }
+
+        //Dropdown select
+
+        public static void DropDownSelect(IWebDriver driver, string element, string value, string elementtype)
+        {
+            if (elementtype == "Id")
+               new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            if (elementtype == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+
+
+        }
+    }
+}
