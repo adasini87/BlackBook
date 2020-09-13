161ea5a46bf7ce4ced1a123282c1ef635ebdd180
adasini87 <71195008+adasini87@users.noreply.github.com>
2020-09-13 17:30:23 -0400

    UITestCompleteNUnitImplementation
    UI Testing completed with NUnit implementation. Unable to hande exception but the code makes sure that all entries created in the computer name are deleted.

---
+++ sste_technical-master/sste_technical-master/ui_testing/BlackBookUI/BlackBookUI/GetMethod.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium.Support.UI;
+
+namespace BlackBookUI
+{
+    class GetMethod
+    {
+        //Get Text
+        public static string GetValue(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+                return driver.FindElement(By.Id(element)).Text;
+            else if (elementType == "Name")
+                return driver.FindElement(By.Name(element)).Text;
+            else if (elementType == "XPath")
+                return driver.FindElement(By.XPath(element)).Text;
+            else return string.Empty;
+        }
+    }
+}
--- sste_technical-master/sste_technical-master/ui_testing/BlackBookUI/BlackBookUI/Program.cs
+++ sste_technical-master/sste_technical-master/ui_testing/BlackBookUI/BlackBookUI/Program.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -49,6 +50,61 @@ namespace BlackBookUI
             SetValue.EnterValue(webDriver, "discontinued", "2021-09-01", "Id");
             SetValue.DropDownSelect(webDriver, "company", "Evans & Sutherland", "Id");
             SetValue.ButtonClick(webDriver, "/html/body/section/form/div/input", "XPath");
+            Thread.Sleep(1000);
+
+            // Search for COmputer Name
+            string name = string.Empty;
+            SetValue.EnterValue(webDriver, "/html/body/section/div[2]/form/input[1]", "BlackBookUITest", "XPath");
+            Thread.Sleep(1000);
+            SetValue.ButtonClick(webDriver, "searchsubmit", "Id");
+            name = GetMethod.GetValue(webDriver, "/html/body/section/table/tbody/tr[1]/td[1]/a", "XPath");
+            if (name == "BlackBookUITest")
+                Console.WriteLine("Computer Name exists");
+              
+            //Modify Discontinued date
+                SetValue.ButtonClick(webDriver, "/html/body/section/table/tbody/tr[1]/td[1]/a","XPath");
+                webDriver.FindElement(By.Id("discontinued")).Clear();
+                SetValue.EnterValue(webDriver, "discontinued", "2022-09-01", "Id");
+                Thread.Sleep(500);
+                SetValue.ButtonClick(webDriver, "/html/body/section/form[1]/div/input", "XPath");
+
+            // Verify modified discontinued date
+                SetValue.EnterValue(webDriver, "/html/body/section/div[2]/form/input[1]", "BlackBookUITest", "XPath");
+                Thread.Sleep(500);
+                SetValue.ButtonClick(webDriver, "searchsubmit", "Id");
+                DateTime date = Convert.ToDateTime(GetMethod.GetValue(webDriver, "/html/body/section/table/tbody/tr/td[3]", "XPath"));
+                if (date == Convert.ToDateTime("2022-09-01"))
+                    Console.WriteLine("DateTime matches exists");
+
+                //delete entries created
+            bool check = true;
+
+                while (check)
+                    {
+
+                //name = GetMethod.GetValue(webDriver, "/html/body/section/table/tbody/tr[1]/td[1]/a", "XPath");
+                //name2 = GetMethod.GetValue(webDriver, "/html/body/section/table/tbody/tr[1]/td[1]/a", "XPath")
+                if (GetMethod.GetValue(webDriver, "/html/body/section/table/tbody/tr[1]/td[1]/a", "XPath") != "BlackBookUITest")
+                {
+                    check = false;
+                    break;
+                }
+                else
+                {
+                    SetValue.ButtonClick(webDriver, "/html/body/section/table/tbody/tr[1]/td[1]/a", "XPath");
+                    Thread.Sleep(500);
+                    SetValue.ButtonClick(webDriver, "/html/body/section/form[2]/input", "XPath");
+                }
+                SetValue.EnterValue(webDriver, "/html/body/section/div[2]/form/input[1]", "BlackBookUITest", "XPath");
+                        Thread.Sleep(500);
+                        SetValue.ButtonClick(webDriver, "searchsubmit", "Id");
+                        
+
+            }
+
+            if (check == false)
+                Console.WriteLine("All Entries Deleted");
+
         }
 
         //[TearDown]
